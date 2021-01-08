28ba709afbea952ea3f39a352eed822a5d2a9425
LH <441686961@qq.com>
2021-01-08 10:05:10 +0800

    feat: 乾坤

---
+++ mock/apps.ts
@@ -0,0 +1,12 @@
+export default {
+  '/app/getApps': [
+    {
+      name: 'app1', // 唯一 id
+      entry: '//localhost:7070', // html entry
+    },
+    // {
+    //   name: 'app2', // 唯一 id
+    //   entry: '//localhost:7002', // html entry
+    // },
+  ],
+};
